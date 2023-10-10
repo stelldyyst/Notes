9dc246cf6853da06b4904f152ebbe64a1f9d9d14
stelldyyst <danmoscowdan@yandex.ru>
2023-10-10 18:49:50 +0300

    Notes
    Notes

---
+++ Notes/Models/AllNotes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Notes.Models;
+
+internal class AllNotes
+{
+    public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+    public AllNotes() =>
+        LoadNotes();
+
+    public void LoadNotes()
+    {
+        Notes.Clear();
+
+
+        string appDataPath = FileSystem.AppDataDirectory;
+
+        IEnumerable<Note> notes = Directory
+
+
+                                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+
+                                    .Select(filename => new Note()
+                                    {
+                                        Filename = filename,
+                                        Text = File.ReadAllText(filename),
+                                        Date = File.GetCreationTime(filename)
+                                    })
+
+
+                                    .OrderBy(note => note.Date);
+
+        foreach (Note note in notes)
+            Notes.Add(note);
+    }
+}
---
+++ Notes/Platforms/MacCatalyst/Program.cs
@@ -0,0 +1,15 @@
+﻿using ObjCRuntime;
+using UIKit;
+
+namespace Notes
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            UIApplication.Main(args, null, typeof(AppDelegate));
+        }
+    }
+}
